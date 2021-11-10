24c87f743230e5d5e9fca1f5642d097be8b4a6a3
LeoKnox <LeoKnox@users.noreply.github.com>
2021-11-10 12:53:11 -0800

    Update svg_group.js

--- svg_group.js
+++ svg_group.js
@@ -1,4 +1,4 @@
 function size_transform() {
   //document.getElementById("shapes").setAttribute('transform','translate(100, 10)');
-  document.getElementById("shapes").transform.translate = 100;
+  document.getElementById("shapes").style.transform = "translate(100px)";
 }
