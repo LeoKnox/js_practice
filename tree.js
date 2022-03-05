f0daa056f739f8e458b4ddf592a56d960c69fd6f
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-04 20:36:55 -0800

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,19 +1,22 @@
-function Bst(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
-  
-  this.add = function(treeValue) {
-    this.left = new Bst(treeValue);
+class Node
+{
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  this.print = function() {
-    console.log(this.left.value);
-  }
-  
 }
 
-a = new Bst(5);
-a.add(3);
-a.print();
+class Bst
+{
+  constructor() {
+    this.head = null;
+  }
+}
+
+a = new Bst;
+a.head = new Node(5);
+console.log(a);
+
+
 
