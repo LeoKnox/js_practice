5aeaa8258ecf841c30bf2278bdf2ea0ef99e019b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-06 16:19:53 -0800

    Update linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -2,33 +2,32 @@ class Node{
   constructor(val) {
     this.val = val;
     this.next = null;
-  };
+  }
 };
   
 class LinkedList{
-    constructor() {
-      head = null;
-      this.count = 0;
-    }
+  constructor() {
+    this.head = null;
+    this.count = 0;
+  }
     
-    function(value) {
-      new_node = Node(value);
-      if (this.head === null) {
-        this.head = new_node;
-        this.count = 1;
-      } else {
-        new_node.next = this.head;
-        this.head = new_node;
-        this.count+=1;
-      }
+  insert(value) {
+    new_node = Node(value);
+    if (this.head === null) {
+      this.head = new_node;
+      this.count = 1;
+    } else {
+      new_node.next = this.head;
+      this.head = new_node;
+      this.count+=1;
     }
+  }
     
-    print() {
-      runner = this.head;
-      while (runner != null) {
-        console.log("Node" + runner.value);
-        runner = runner.next;
-      }
+  display() {
+    let runner = this.head;
+    while (runner != null) {
+      console.log("Node" + runner.value);
+      runner = runner.next;
     }
-  };
+  }
 }
