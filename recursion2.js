4ba892a8876817057163eccc67150231eb18a585
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-07 12:04:32 -0800

    Create recursion2.js

---
+++ recursion2.js
@@ -0,0 +1,18 @@
+function power(num, pwr) {
+  if (pwr == 0) {
+    return(1);
+  } else {
+    return(num*power(num, pwr-1));
+  }
+}
+
+function factorial(num) {
+  if (num === 0) {
+    return(1);
+  } else {
+    return(num*factorial(num-1));
+  }
+}
+
+console.log(factorial(3));
+console.log(power(2,4));
