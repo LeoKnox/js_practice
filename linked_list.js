4db68441d24c9df7a99c06c481fdf2940ed28190
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-06 16:39:34 -0800

    Update linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -12,7 +12,7 @@ class LinkedList{
   }
     
   insert(value) {
-    new_node = Node(value);
+    let new_node = new Node(value);
     if (this.head === null) {
       this.head = new_node;
       this.count = 1;
@@ -22,12 +22,34 @@ class LinkedList{
       this.count+=1;
     }
   }
+  
+  find(value) {
+    let runner = this.head;
+    while (runner != null) {
+      if (runner.val == value) {
+        console.log("True");
+        break;
+      } else {
+        runner = runner.next;
+      }
+    }
+    console.log("false");
+  }
     
   display() {
     let runner = this.head;
     while (runner != null) {
-      console.log("Node" + runner.value);
+      console.log("Node " + runner.val);
       runner = runner.next;
     }
   }
 }
+
+
+let a = new LinkedList;
+a.display();
+a.insert(5);
+a.insert(9);
+a.find(5);
+a.find(10);
+a.display();
